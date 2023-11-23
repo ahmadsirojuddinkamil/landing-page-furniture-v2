bae188b8cf6f980b87247b2e90d68839b284983f
ahmadsirojuddinkamil <ahmadsirojuddinkamil@gmail.com>
2023-11-23 21:18:05 +0700

    source code landing page furniture v2

--- admin/updateimage.php
+++
@@ -1,5 +0,0 @@
-<?php
- 
-				echo $_FILES["file"]["name"];
-				echo $_POST["yazi"];
-?>
--- gallery.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo '<div style="text-align: center;"><h1>Bu Sayfa Yapım Aşamasındadır</h1><br> Ana Sayfaya Yönlendiriliyorsunuz....</div>';
-@header("refresh:3;url=anasayfa.html"); 
-?>
